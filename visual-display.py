ed9cfa2f18e693d44a9c40d68b4e8fd6cbcf8cf0
sue <1609587839@qq.com>
2024-03-03 14:55:52 +0800

    'second'

--- visual-display.py
+++ visual-display.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import pandas as pd
 
 # 表示console控制台下 读取
-data = pd.read_csv('.//name_score.csv')
+data = pd.read_csv('analysis_data/name_score.csv')
 
 fig = plt.figure(figsize=(5,6))
 fig.subplots_adjust(hspace=0.3)
@@ -11,4 +11,8 @@ ax1 = fig.add_subplot(211)
 sns.lineplot(data=data,x='math',y='physical',hue='gender',errorbar=('ci', 0))
 ax1.legend(loc='upper left',frameon=False)
 ax2 = fig.add_subplot(212)
-sns.lineplot(data=data,x='math',y='chemistry',errorbar=('ci', 0))
+# sns.lineplot(data=data,x='math',y='chemistry',errorbar=('ci', 0))
+def hist_plt():
+    sns.histplot(bins=5,data=data,x='math',hue='gender',palette=sns.color_palette('hls',2),kde=True)
+hist_plt()
+plt.show()
